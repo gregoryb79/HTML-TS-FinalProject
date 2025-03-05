011eb02ae3ff741052f6638ec5874c2401d6ecd4
gregoryb79 <gregoryb79@gmail.com>
2025-03-05 18:00:40 +0200

    tests and prescriptions added

---
+++ dist/prescriptions.js
@@ -0,0 +1,27 @@
+import { getCurrentPatient, getPatient, getPatientPrescriptions } from "./model.js";
+export function init(namePlacer, prescriptionList) {
+    const patientID = getCurrentPatient();
+    if (patientID === "") {
+        window.location.href = "./login.html";
+    }
+    const today = new Date;
+    const currPatient = getPatient();
+    namePlacer.textContent = `${currPatient.name} ${currPatient.surname}`;
+    const prescriptions = getPatientPrescriptions();
+    if (prescriptions.length > 0) {
+        prescriptionList.innerHTML = "";
+        for (const prescription of prescriptions) {
+            const liEL = document.createElement("li");
+            const date = prescription.date.toLocaleString("en-IL", { year: "numeric", month: "2-digit", day: "2-digit" });
+            liEL.dataset.id = prescription.id;
+            liEL.textContent = `${date} ${prescription.drug}`;
+            prescriptionList.appendChild(liEL);
+        }
+    }
+    else {
+        prescriptionList.innerHTML = "";
+        const liEL = document.createElement("li");
+        liEL.textContent = `No prescriptions to display.`;
+        prescriptionList.appendChild(liEL);
+    }
+}
